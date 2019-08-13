ceec7fa1dd02c69c7b58eb0ca5cbf72e0f23d366
procir <yoshi-s-0202@docomo.ne.jp>
2019-08-13 21:09:10 +0900

    echo追加

---
+++ form20.php
@@ -0,0 +1,18 @@
+<?php
+echo 'git';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Git</title>
+</head>
+<body>
+<h1>
+<font size='5'>入力してください</font>
+</h1>
+<form action='post20.php' method='post'>
+<input type='text' name='post'>
+<input type='submit' value='送信'>
+</form>
+</body>
+</html>
---
+++ post20.php
@@ -0,0 +1,13 @@
+<?php
+$message = $_POST['post'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Git</title>
+</head>
+<body>
+<p><a href='get20.php?key=<?php echo $message; ?>'>飛ぶ</a></p>
+</body>
+</html>
